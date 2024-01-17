58adf1c53770100658b5b5029d6c5c75004e6089
zbornak <zbornak@protonmail.com>
2024-01-17 11:09:01 +0000

    Added test data

--- SumoDEX/ContentView.swift
+++ SumoDEX/ContentView.swift
@@ -7,28 +7,39 @@
 
 import SwiftUI
 
-struct Rikishi: Identifiable {
+struct Rikishi: Comparable, Identifiable {
     let id = UUID()
     var name: String
+    var kanji: String
     var birthName: String
     var nationality: String
     var hometown: String
     var age: Int
+    var height: Double
     var weight: Int
     var currentRank: String
     var highestRank: String
     var stable: String
+    var championshipsWon : Int
+    var specialPrizes: [String]
+    
+    static func <(lhs: Rikishi, rhs: Rikishi) -> Bool {
+        lhs.name < rhs.name
+    }
 }
 
 struct ContentView: View {
-    var activeRikishiList = ["Gonoyama", "Terunofuji", "Ura", "Hoshoryu"]
+    let testRikishi = [
+        Rikishi(name: "Hoshoryu", kanji: "豊昇龍", birthName: "Sugarragchaagiin Byambasuren", nationality: "Mongolian", hometown: "Ulaanbaatar", age: 24, height: 1.87, weight: 140, currentRank: "Ozeki", highestRank: "Ozeki", stable: "Tatsunami", championshipsWon: 2, specialPrizes: ["Technique", "Fighting Spirit"]),
+        Rikishi(name: "Endo", kanji: "遠藤聖大", birthName: "Shota Endo", nationality: "Japanese", hometown: "Anamizu", age: 33, height: 1.83, weight: 148, currentRank: "Maegashira", highestRank: "Komusubi", stable: "Oitekaze", championshipsWon: 1, specialPrizes: ["Fighting Spirit", "Technique", "Outstanding Performance"])
+    ].sorted()
     
     var body: some View {
         NavigationStack {
-            List(activeRikishiList, id: \.self) {
-                Text($0)
+            List(testRikishi) { rikishi in
+                Text("\(rikishi.name)")
             }
-            .navigationTitle("相撲デクス")
+            .navigationTitle("相撲デックス")
         }
     }
 }
