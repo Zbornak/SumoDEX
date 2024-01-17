874b8e7529d68d6d0391758986071892a5f715d2
zbornak <zbornak@protonmail.com>
2024-01-17 09:49:21 +0000

    Initial Commit

---
+++ SumoDEX/SumoDEXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SumoDEXApp.swift
+//  SumoDEX
+//
+//  Created by Mark Strijdom on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SumoDEXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
