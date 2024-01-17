defccf0e2bd1d6d42d9330168889790c61931ddf
zbornak <zbornak@protonmail.com>
2024-01-17 10:11:18 +0000

    First commit, created basic list and rikishi struct

--- SumoDEX/ContentView.swift
+++ SumoDEX/ContentView.swift
@@ -7,15 +7,29 @@
 
 import SwiftUI
 
+struct Rikishi: Identifiable {
+    let id = UUID()
+    var name: String
+    var birthName: String
+    var nationality: String
+    var hometown: String
+    var age: Int
+    var weight: Int
+    var currentRank: String
+    var highestRank: String
+    var stable: String
+}
+
 struct ContentView: View {
+    var activeRikishiList = ["Gonoyama", "Terunofuji", "Ura", "Hoshoryu"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(activeRikishiList, id: \.self) {
+                Text($0)
+            }
+            .navigationTitle("相撲デクス")
         }
-        .padding()
     }
 }
 
