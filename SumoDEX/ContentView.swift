fe8057c01a1171c7e68fb3df832997fcb6b65344
zbornak <zbornak@protonmail.com>
2024-01-19 14:45:31 +0000

    Trying web scraping instead of using wiki api

--- SumoDEX/ContentView.swift
+++ SumoDEX/ContentView.swift
@@ -30,12 +30,6 @@ struct Rikishi: Comparable, Identifiable {
 }
 
 struct ContentView: View {
-    enum LoadingState {
-        case loading, loaded, failed
-    }
-    
-    @State private var loadingState = LoadingState.loading
-    @State private var pages = [Page]()
     
     let testRikishi = [
         Rikishi(name: "Hoshoryu", kanji: "豊昇龍", birthName: "Sugarragchaagiin Byambasuren", nationality: "Mongolian", hometown: "Ulaanbaatar", age: 24, height: 1.87, weight: 140, currentRank: "Ozeki", highestRank: "Ozeki", stable: "Tatsunami", championshipsWon: 2, specialPrizes: ["Technique", "Fighting Spirit"], notes: ""),
@@ -46,41 +40,15 @@ struct ContentView: View {
         NavigationStack {
             Form {
                 Section("Active Rikishi") {
-                    switch loadingState {
-                    case .loaded:
-                        ForEach(pages, id: \.pageid) { page in
-                            Text(page.title)
-                                    .font(.headline)
-                                }
-                    case .loading:
-                        Text("Loading…")
-                    case .failed:
-                        Text("Please try again later.")
+                    List {
+                        ForEach(testRikishi, id: \.id) { rikishi in
+                            Text(rikishi.name)
+                        }
                     }
                 }
             }
             .navigationTitle("相撲デックス")
         }
-        .task {
-            await fetchActiveRikishi()
-        }
-    }
-    
-    func fetchActiveRikishi() async {
-        let urlString = "https://en.wikipedia.org/w/api.php?action=query&titles=List_of_active_sumo_wrestlers&prop=revisions&rvprop=content&format=json"
-        guard let url = URL(string: urlString) else {
-                print("Bad URL: \(urlString)")
-                return
-            }
-
-            do {
-                let (data, _) = try await URLSession.shared.data(from: url)
-                let items = try JSONDecoder().decode(Result.self, from: data)
-                pages = items.query.pages.values.sorted { $0.title < $1.title }
-                loadingState = .loaded
-            } catch {
-                loadingState = .failed
-            }
     }
 }
 
