7737a0df37a4a547abac2050616d6af50bfabb46
zbornak <zbornak@protonmail.com>
2024-01-22 14:33:43 +0000

    Working on filtering array for just rikishi names

--- SumoDEX/ContentView.swift
+++ SumoDEX/ContentView.swift
@@ -11,19 +11,6 @@ import SwiftUI
 struct Rikishi: Comparable, Identifiable {
     let id = UUID()
     var name: String
-    var kanji: String
-    var birthName: String
-    var nationality: String
-    var hometown: String
-    var age: Int
-    var height: Double
-    var weight: Int
-    var currentRank: String
-    var highestRank: String
-    var stable: String
-    var championshipsWon : Int
-    var specialPrizes: [String]
-    var notes: String
     
     static func <(lhs: Rikishi, rhs: Rikishi) -> Bool {
         lhs.name < rhs.name
@@ -31,24 +18,18 @@ struct Rikishi: Comparable, Identifiable {
 }
 
 struct ContentView: View {
-    
-    let testRikishi = [
-        Rikishi(name: "Hoshoryu", kanji: "豊昇龍", birthName: "Sugarragchaagiin Byambasuren", nationality: "Mongolian", hometown: "Ulaanbaatar", age: 24, height: 1.87, weight: 140, currentRank: "Ozeki", highestRank: "Ozeki", stable: "Tatsunami", championshipsWon: 2, specialPrizes: ["Technique", "Fighting Spirit"], notes: ""),
-        Rikishi(name: "Endo", kanji: "遠藤聖大", birthName: "Shota Endo", nationality: "Japanese", hometown: "Anamizu", age: 33, height: 1.83, weight: 148, currentRank: "Maegashira", highestRank: "Komusubi", stable: "Oitekaze", championshipsWon: 1, specialPrizes: ["Fighting Spirit", "Technique", "Outstanding Performance"], notes: "")
-    ].sorted()
-    
+    @State private var searchText = ""
     @State private var rikishiInfoArray = [String]()
     
     var body: some View {
         NavigationStack {
-            Form {
-                Section("Active Rikishi") {
-                    List {
-                        Text("hello")
-                    }
+            List {
+                ForEach(rikishiInfoArray, id: \.self) { rikishi in
+                    Text(rikishi)
                 }
             }
             .navigationTitle("相撲デックス")
+            .searchable(text: $searchText)
             .task {
                 await rikishiInfoArray = fetchActiveRikishiData()
             }
@@ -73,6 +54,12 @@ struct ContentView: View {
         }
         
         return rikishiInfoArray
+    }
+    
+    func filterArrayForNames(from arrayToFilter: [String]) -> [String] {
+        let newArray = [String]()
+        
+        return newArray
     }
 }
 
