ee75ddc8f2fba6dae2b29ba3aa40680df60bcc81
zbornak <zbornak@protonmail.com>
2024-01-22 15:17:21 +0000

    Trying to use regex to filter array (to find the japanese chars)

--- SumoDEX/ContentView.swift
+++ SumoDEX/ContentView.swift
@@ -57,14 +57,13 @@ struct ContentView: View {
     }
     
     func filterArrayForNames(from arrayToFilter: [String]) -> [String] {
-        var newArray = [String]()
-        for i in 0...arrayToFilter.count {
-            if i.isMultiple(of: 7) {
-                newArray.append(arrayToFilter[i])
-            }
+        do {
+            let kanji = try Regex("/[\u{3040}-\u{30ff}\u{3400}-\u{4dbf}\u{4e00}-\u{9fff}\u{f900}-\u{faff}\u{ff66}-\u{ff9f}]/")
+            var newArray = arrayToFilter.filter { !$0.contains(kanji) }
+            return newArray
+        } catch {
+            return ["Error: REGEX filtering failed"]
         }
-        
-        return newArray
     }
 }
 
