3ce78acd1488766ede5707c2c10c42c4812a5237
zbornak <zbornak@protonmail.com>
2024-01-23 09:46:26 +0000

    Added rikishi detail view

--- SumoDEX/RikishiDetailView.swift
+++ SumoDEX/RikishiDetailView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct RikishiDetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Rikishi detail")
     }
 }
 
