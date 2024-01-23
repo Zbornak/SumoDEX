aa696154ecfaf9a888f4ce0c86b087b78624e10b
zbornak <zbornak@protonmail.com>
2024-01-23 09:46:04 +0000

    Added rikishi detail view

---
+++ SumoDEX/RikishiDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  RikishiDetailView.swift
+//  SumoDEX
+//
+//  Created by Mark Strijdom on 23/01/2024.
+//
+
+import SwiftUI
+
+struct RikishiDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RikishiDetailView()
+}
