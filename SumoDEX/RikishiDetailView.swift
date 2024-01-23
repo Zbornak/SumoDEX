2841bb9a820fb0e3ceef63301d0e81375d7bbfc7
zbornak <zbornak@protonmail.com>
2024-01-23 15:24:58 +0000

    Basic styling to detail view

--- SumoDEX/RikishiDetailView.swift
+++ SumoDEX/RikishiDetailView.swift
@@ -12,16 +12,42 @@ struct RikishiDetailView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                Image(systemName: "figure.wrestling")
-                    .font(.largeTitle)
-                Text(rikishi.currentRank)
-                Text(rikishi.debut)
-                Text(rikishi.heya)
-                Text(rikishi.birthdate)
-                Text(rikishi.hometown)
-                Text(rikishi.information)
-                    .padding()
+                ZStack {
+                    Image(systemName: "oval").font(.system(size: 86)).offset(x: 0, y: 8)
+                    HStack {
+                        Image(systemName: "figure.wrestling")
+                            .font(.largeTitle)
+                        Image(systemName: "figure.wrestling").scaleEffect(CGSize(width: -1.0, height: 1.0))
+                            .font(.largeTitle)
+                    }
+                }
+                .padding()
+                HStack {
+                    Text("Current rank:").fontWeight(.bold)
+                    Text(rikishi.currentRank)
+                }
+                HStack {
+                    Text("Debut:").fontWeight(.bold)
+                    Text(rikishi.debut)
+                }
+                HStack {
+                    Text("Heya:").fontWeight(.bold)
+                    Text(rikishi.heya)
+                }
+                HStack {
+                    Text("Birthdate:").fontWeight(.bold)
+                    Text(rikishi.birthdate)
+                }
+                HStack {
+                    Text("Hometown:").fontWeight(.bold)
+                    Text(rikishi.hometown)
+                }
+                .padding(.bottom)
+                Text("Information").fontWeight(.bold)
+                Text(rikishi.information).multilineTextAlignment(.center)
+                
             }
+            .padding()
             .navigationTitle(rikishi.name)
         }
     }
