0545cc9889289c67ac8f86515bf11b420404f848
zbornak <zbornak@protonmail.com>
2024-01-24 10:38:57 +0000

    Added ZStack and image

--- SumoDEX/RikishiDetailView.swift
+++ SumoDEX/RikishiDetailView.swift
@@ -11,33 +11,36 @@ struct RikishiDetailView: View {
     let rikishi: Rikishi
     var body: some View {
         NavigationStack {
-            VStack {
-                HStack {
-                    Image(systemName: "figure.wrestling").font(.largeTitle)
-                    Image(systemName: "figure.wrestling")
-                        .scaleEffect(CGSize(width: -1.0, height: 1.0))
-                        .font(.largeTitle)
+            ZStack {
+                Image("gyoji").resizable().scaledToFill().opacity(0.2).blur(radius: 3.0)
+                VStack {
+                    HStack {
+                        Image(systemName: "figure.wrestling").font(.largeTitle)
+                        Image(systemName: "figure.wrestling")
+                            .scaleEffect(CGSize(width: -1.0, height: 1.0))
+                            .font(.largeTitle)
+                    }
+                    .padding()
+                    
+                    Text("Current rank:").fontWeight(.bold)
+                    Text(rikishi.currentRank).padding(.bottom)
+                    
+                    Text("Debut:").fontWeight(.bold)
+                    Text(rikishi.debut).padding(.bottom)
+                    
+                    Text("Heya:").fontWeight(.bold)
+                    Text(rikishi.heya).padding(.bottom)
+                    
+                    Text("Birthdate:").fontWeight(.bold)
+                    Text(rikishi.birthdate).padding(.bottom)
+                    
+                    Text("Hometown:").fontWeight(.bold)
+                    Text(rikishi.hometown).padding(.bottom)
+                    
+                    Text("Information").fontWeight(.bold)
+                    Text(rikishi.information).multilineTextAlignment(.center)
+                    
                 }
-                .padding()
-                
-                Text("Current rank:").fontWeight(.bold)
-                Text(rikishi.currentRank).padding(.bottom)
-                
-                Text("Debut:").fontWeight(.bold)
-                Text(rikishi.debut).padding(.bottom)
-                
-                Text("Heya:").fontWeight(.bold)
-                Text(rikishi.heya).padding(.bottom)
-                
-                Text("Birthdate:").fontWeight(.bold)
-                Text(rikishi.birthdate).padding(.bottom)
-                
-                Text("Hometown:").fontWeight(.bold)
-                Text(rikishi.hometown).padding(.bottom)
-                
-                Text("Information").fontWeight(.bold)
-                Text(rikishi.information).multilineTextAlignment(.center)
-                
             }
             .padding()
             .navigationTitle(rikishi.name)
