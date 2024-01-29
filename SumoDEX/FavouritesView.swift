0096ac170181cef3d289dba46b90c57019551258
zbornak <zbornak@protonmail.com>
2024-01-29 13:37:32 +0000

    Added favourites view

---
+++ SumoDEX/FavouritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavouritesView.swift
+//  SumoDEX
+//
+//  Created by Mark Strijdom on 29/01/2024.
+//
+
+import SwiftUI
+
+struct FavouritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavouritesView()
+}
